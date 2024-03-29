4d6719d07a7e0c3f018fda3f375a5e9ea18b3f6a
Happysttim <gmlsdyd1023@naver.com>
2024-03-30 04:43:51 +0900

    add test

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,60 @@
 import JSONBrowser from '../src/json-browser';
 
-const jsonString = '{"arrayTest": [{"Name": "NOH", "Age": 25, "HasNum": [1,2,3,4,5]},{"Name": "Kim", "Age": 23}]}';
-const browser = JSONBrowser.parse(jsonString);
+interface User {
+    id: string
+    name: string
+    age: number
+    gender: string
+}
 
-// console.log(JSON.parse(jsonString));
-console.log(browser.json["arrayTest"][0]["HasNum"]);
+interface UserInfo {
+    user: User
+    level: number
+    isOnline: boolean
+}
+
+interface Game {
+    userInfo: UserInfo[],
+}
+
+const game: Game = {
+    userInfo: [
+        {
+            user: {
+                id: 'user2',
+                name: 'Pak User',
+                age: 20,
+                gender: 'male',
+            },
+            level: 20,
+            isOnline: true
+        },
+        {
+            user: {
+                id: 'user1',
+                name: 'Kim User',
+                age: 10,
+                gender: 'female',
+            },
+            level: 99,
+            isOnline: false
+        },
+        {
+            user: {
+                id: 'user3',
+                name: 'Chai User',
+                age: 30,
+                gender: 'female',
+            },
+            level: 10,
+            isOnline: true
+        }
+    ]
+};
+
+const jsonString = JSON.stringify(game);
+console.log(jsonString);
+
+const browser = JSONBrowser.parse(jsonString);
+console.log(browser.json);
+console.log(browser.next('userInfo').index(0).get<User>("user"));
